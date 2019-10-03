e78637e9cbdd3ed4d7b75527b3ad4e712c57e0d0
ClearlyElevated <51843882+ClearlyElevated@users.noreply.github.com>
2019-10-02 22:22:41 -0700

    change to readme
    removed the how to texted and added readme.md

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-//To make a Discord Bot Go to https://discordapp.com/developers and create a bot. Go to Bot and add bot!
-then go to https://glitch.com and signin and click on New Peoject and go to #hello-express.
-Then go to package.json add the package Discord.js
-Then go to .env file and erase everything and type TOKEN=(your bots token)
-then create a new file call it config.json
-Type: 
-{
-"prefix": "!"
-}
-then go to server.js and type this
-
-
 const Discord = require('discord.js');
 const express = require('express');
 const config = require('./config.json');
